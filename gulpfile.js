2f7323083f48e30625ec05c7118cdd2386e27ad8
mixture <turkeevm@gmail.com>
2018-10-11 17:36:39 +0300

    remove gulp; migrate to babel 7; should fix github complains on security

--- gulpfile.js
+++
@@ -1,7 +0,0 @@
-const gulp = require('gulp');
-const babel = require('gulp-babel');
-
-gulp.task('build', () =>
-    gulp.src('src/**/*.js')
-        .pipe(babel())
-        .pipe(gulp.dest('lib/')));
